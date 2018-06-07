a1702d9f00bea359e792e2f263d50774f3ad75d3
xank <xank@qq.com>
2018-06-07 22:42:54 +0800

    package

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -36,7 +36,7 @@ module.exports = {
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src'),
-      'package': resolve('package')
+      'package': resolve('src/package')
     }
   },
   module: {
