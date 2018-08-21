047d87611f3841bf59efcca9908b38dc376dfb09
xank <xank@qq.com>
2018-08-22 00:13:47 +0800

    fire

--- src/pages/explorer/firetime.js
+++ src/pages/explorer/firetime.js
@@ -29,15 +29,128 @@ const FIRE_TYPE = [
   }
 ]
 
-const DATA = [{
-  time: 0,
-  fire: [{
+const FIRE_POSITION = [
+  {
+    label: '城堡中央',
+    value: 'A1'
+  },
+  {
+    label: '城堡左侧',
+    value: 'A2'
+  },
+  {
+    label: '城堡右侧',
+    value: 'A3'
+  },
+  {
+    label: '左侧',
+    value: 'B1'
+  },
+  {
+    label: '右侧',
+    value: 'B2'
+  },
+  {
+    label: '背部',
+    value: 'C1'
+  }
+]
 
-  }]
-}]
+const DATA = [
+  {
+    time: 0,
+    fire: [
+      {
+        type: FIRE_TYPE_VAR.LINE,
+        position: 'B1',
+        color: 'white'
+      },
+      {
+        type: FIRE_TYPE_VAR.LINE,
+        position: 'B2',
+        color: 'white'
+      }
+    ]
+  },
+  {
+    time: 1323,
+    fire: [
+      {
+        type: FIRE_TYPE_VAR.SPRAY,
+        position: 'C1',
+        num: 2
+      },
+      {
+        type: FIRE_TYPE_VAR.SPRAY,
+        position: 'C2'
+      },
+      {
+        type: FIRE_TYPE_VAR.SPRAY,
+        position: 'C3'
+      }
+    ]
+  },
+  {
+    time: 2512,
+    fire: [
+      {
+        type: FIRE_TYPE_VAR.SPRAY,
+        position: 'B1',
+        num: 2
+      },
+      {
+        type: FIRE_TYPE_VAR.SPRAY,
+        position: 'B2'
+      }
+    ]
+  },
+  {
+    time: 2574,
+    fire: [
+      {
+        type: FIRE_TYPE_VAR.SPRAY,
+        position: 'B1',
+        num: 2
+      },
+      {
+        type: FIRE_TYPE_VAR.SPRAY,
+        position: 'B2'
+      }
+    ]
+  },
+  {
+    time: 2644,
+    fire: [
+      {
+        type: FIRE_TYPE_VAR.SPRAY,
+        position: 'B1',
+        num: 2
+      },
+      {
+        type: FIRE_TYPE_VAR.SPRAY,
+        position: 'B2'
+      }
+    ]
+  },
+  {
+    time: 2721,
+    fire: [
+      {
+        type: FIRE_TYPE_VAR.SPRAY,
+        position: 'B1',
+        num: 2
+      },
+      {
+        type: FIRE_TYPE_VAR.SPRAY,
+        position: 'B2'
+      }
+    ]
+  }
+]
 
 export {
   FIRE_TYPE_VAR,
   FIRE_TYPE,
-  DATA
+  DATA,
+  FIRE_POSITION
 }
--- src/pages/explorer/firetime.vue
+++ src/pages/explorer/firetime.vue
@@ -1,25 +1,46 @@
 <style lang='stylus'>
 </style>
 <template>
-  <div>
-
-      
+  <div class="page bg--gray">
+    <dm-content>
+      <dm-card>
+        <el-table :data="DATA">
+          <el-table-column prop="time" label="时间" width="180"></el-table-column>
+          <el-table-column prop="time" label="时间" width="180">
+            <template slot-scope="scope">
+              {{scope.row.time / 25}}
+            </template>
+          </el-table-column>
+          <el-table-column label="烟花类型">
+            <template slot-scope="scope">
+              <div v-for="item in scope.row.fire">
+                {{item.type}}: {{item.position}} - {{item.num || 1}}
+              </div>
+            </template>
+          </el-table-column>
+        </el-table>
+      </dm-card>
+    </dm-content>
   </div>
 </template>
 
 <script>
+import { DATA } from './firetime'
+
 export default {
   components: {},
 
   props: {},
 
   data() {
-    return {}
+    return {
+      DATA
+    }
   },
 
   computed: {},
 
-  mounted() {},
+  mounted() { },
 
   methods: {}
 }
